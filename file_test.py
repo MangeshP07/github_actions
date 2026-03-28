70c5b8b75ec363c9235d7904628e6491a0566a41
Mangesh Pise <impise07@gmail.com>
2026-03-28 01:02:07 +0100

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -2,6 +2,7 @@ def test_calc_addition():
     output = 2+4
     assert output == 6
 
+
 def test_calc_substraction():
     # ३. कमेंटच्या मागे बरोबर ४ स्पेस आहेत (E114 फिक्स करण्यासाठी)
     output = 2 - 4
@@ -18,3 +19,4 @@ def test_coucou():
     # ५. '=' च्या दोन्ही बाजूला स्पेस दिला आहे (E225 फिक्स करण्यासाठी)
     output = 'hello'
     assert output == 'hello'
+    
